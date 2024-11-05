dc8caca5c31e04311f0155902ee28cdec93b2ae6
GlaucoTolotto <glaucotolotto@gmail.com>
2024-11-05 09:18:40 -0300

    Adicionando funcao para detectar faces

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from cv2_plt_imshow import cv2_plt_imshow, plt_format
 import os 
 import zipfile
@@ -11,12 +12,38 @@ zip_object = zipfile.ZipFile(file = path, mode = 'r')
 zip_object.extractall('./')
 zip_object.close()
 
-print(os.listdir('yalefaces/train'))
-
+# Convertendo imagem para cinza
 imagem_teste = 'yalefaces/train/subject01.leftlight.gif'
 imagem = Image.open(imagem_teste).convert('L')
 type(imagem)
 
 imagem_np = np.array(imagem, 'uint8')
 cv2_plt_imshow(imagem_np)
-print(imagem_np.shape)
+
+network = cv2.dnn.readNetFromCaffe('weights/deploy.prototxt.txt', 'weights/res10_300x300_ssd_iter_140000.caffemodel')
+
+def detecta_face(network, path_imagem, conf_min = 0.7):
+  imagem = Image.open(path_imagem).convert('L')
+  imagem = np.array(imagem, 'uint8')
+  imagem = cv2.cvtColor(imagem, cv2.COLOR_GRAY2BGR)
+  (h, w) = imagem.shape[:2]
+  blob = cv2.dnn.blobFromImage(cv2.resize(imagem, (100, 100)), 1.0, (100,100), (104.0, 117.0, 123.0))
+  network.setInput(blob)
+  deteccoes = network.forward()
+
+  face = None
+  for i in range(0, deteccoes.shape[2]):
+    confianca = deteccoes[0, 0, i, 2]
+    if confianca > conf_min:
+      bbox = deteccoes[0, 0, i, 3:7] * np.array([w, h, w, h])
+      (start_x, start_y, end_x, end_y) = bbox.astype('int')
+      roi = imagem[start_y:end_y, start_x:end_x]
+      roi = cv2.resize(roi, (60,80))
+      cv2.rectangle(imagem, (start_x, start_y), (end_x, end_y), (0,255,0), 2)
+      face = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+  return face, imagem
+teste_imagem = 'yalefaces/train/subject01.sad.gif'
+face, imagem = detecta_face(network, teste_imagem)
+cv2_plt_imshow(imagem)
+plt.show()
+print(face.shape)
